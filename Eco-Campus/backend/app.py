751db2d23358bf445e6a64c29dd17f2848e34144
Nishaad-Paunikar <nishaad.paunikar@gmail.com>
2025-09-16 12:06:52 +0530

    Updated app.py

--- Eco-Campus/backend/app.py
+++ Eco-Campus/backend/app.py
@@ -1,16 +1,19 @@
-from flask import Flask
-from backend.utils.db import init_app, db
-from backend.models.models import User, Quiz, Leaderboard
+from flask import Flask, jsonify
+from utils.db import get_db_connection
 
-def create_app():
-    app = Flask(__name__)
-    init_app(app)
+app = Flask(__name__)
 
-    with app.app_context():
-        db.create_all()  # create tables in DB
-
-    return app
+@app.route("/test-db")
+def test_db():
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT DATABASE();")
+        db_name = cursor.fetchone()
+        conn.close()
+        return jsonify({"status": "success", "database": db_name[0]})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
